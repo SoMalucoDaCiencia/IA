05d13426d116ba0992db5a667e5923c10c69fc77
taisarantes <tais.reis.arantes@gmail.com>
2024-03-24 20:50:26 -0300

    Funções de vitória definidas

--- Ep1/connect_four.py
+++ Ep1/connect_four.py
@@ -1,49 +1,79 @@
-from enum import Enum
-            
+# Classe para cada círculo/posição do tabuleiro
 class Circle:
     def __init__(self, x, y, player):
         self.X = x # int
         self.Y = y # int
         self.player = player # bool
 
+# Classe do jogo inteiro
 class Schema:
+    COLLUMNS = 7
+    ROWS = 6
+
     def __init__(self):
         self.contentSize = 0  # int
-        self.content = []  # []Square
+        self.content = []  # []Circle
 
-        for x in range(0, 7):
-            for y in range(0, 6):
+        # Cria um tabuleiro com 42 espaços vazios
+        for x in range(0, self.COLLUMNS):
+            for y in range(0, self.ROWS):
                self.content.append(Circle(x, y, None))
 
+    # Retorna as posições do tabuleiro
     def getAt(self, x, y):
-        if y < 0 or y == 6 or x < 0 or x == 7:
+        if y < 0 or y == self.ROWS or x < 0 or x == self.COLLUMNS:
             raise Exception("Invalid Index (getAt)")
 
-        index = (6 - (y + 1)) * 7 + x
+        index = (self.ROWS - (y + 1)) * self.COLLUMNS + x
         if len(self.content) > index:
             return self.content[index]
         else:
             raise Exception("Invalid Index (getAt)")
-    
+
+    # Cria as posições do tabuleiro   
     def setAt(self, x, y, player):
-        if y < 0 or y == 6 or x < 0 or x == 7:
+        if y < 0 or y == self.ROWS or x < 0 or x == self.COLLUMNS:
             raise Exception("Invalid Index (setAt)")
 
-        index = (6 - (y + 1)) * 7 + x
+        index = (self.ROWS - (y + 1)) * self.COLLUMNS + x
         if len(self.content) > index:
             self.content[index].player = player 
         else:
             raise Exception("Invalid Index (setAt)")
-        
-    def print(self):
-        for y in range(5, -1, -1):
+    
+    # Printa o tabuleiro    
+    def printBoard(self):
+        for y in range(self.ROWS-1, -1, -1):
             board = ""
-            for x in range(0, 7):
+            for x in range(0, self.COLLUMNS):
                circle = self.getAt(x, y)
                if circle.player == True:
-                   board += '\033[96m' + " ● " + '\033[0m'
+                   board += '\033[96m' + " ● " + '\033[0m' # Player azul 
                elif circle.player == False:
-                   board += '\033[93m' + " ● " + '\033[0m'
+                   board += '\033[93m' + " ● " + '\033[0m' # Player amarelo
                else:
                    board += " • "
             print(board)
+
+    # Define as posições de vitória
+    def wonHorizontal(self, player):
+        # Verifica as posições horizontais
+        for y in range(0, self.ROWS):  
+             for x in range(0, self.COLLUMNS-3): 
+                if self.getAt(x, y).player == player and self.getAt(x+1, y).player == player and self.getAt(x+2, y).player == player and self.getAt(x+3, y).player == player:
+                    return True
+                
+    def wonVertical(self, player):
+        # Verifica as posições verticais
+        for x in range(0, self.COLLUMNS): 
+            for y in range(0, self.ROWS-3):
+                if self.getAt(x, y).player == player and self.getAt(x, y+1).player == player and self.getAt(x, y+2).player == player and self.getAt(x, y+3).player == player:
+                    return True
+
+    def wonDiagonal(self, player):       
+        # Verifica as posições diagonais
+        for x in range(0, self.COLLUMNS-3): 
+            for y in range(0, self.ROWS-3):
+                if self.getAt(x, y).player == player and self.getAt(x + 1, y + 1).player == player and self.getAt(x + 2, y + 2).player == player and self.getAt(x + 3, y + 3).player == player:
+                    return True
+                
--- Ep1/main.py
+++ Ep1/main.py
@@ -1,6 +1,9 @@
 from connect_four import Schema
 
 jogo = Schema()
-# jogo.setAt(0, 0, False)
-# print(jogo.content[35].player)
-jogo.print()
+jogo.setAt(3, 1, False)
+jogo.setAt(4, 2, False)
+jogo.setAt(5, 3, False)
+jogo.setAt(6, 4, False)
+print(jogo.wonDiagonal(False))
+jogo.printBoard()
